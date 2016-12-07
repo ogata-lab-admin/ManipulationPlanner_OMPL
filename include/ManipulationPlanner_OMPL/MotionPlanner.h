5878eed0c0b8789c63aba8f8776ebdd8465c19f6
ogata <futail2a@gmail.com>
2016-12-07 18:04:53 +0900

    rm sample header

--- include/ManipulationPlanner_OMPL/MotionPlanner.h
+++ include/ManipulationPlanner_OMPL/MotionPlanner.h
@@ -3,6 +3,7 @@
 
 #include <ompl/geometric/SimpleSetup.h>
 
+//Planning Methods
 #include <ompl/geometric/planners/prm/PRM.h>
 #include <ompl/geometric/planners/rrt/RRTstar.h>
 #include <ompl/geometric/planners/rrt/LBTRRT.h>
@@ -12,6 +13,7 @@
 #include <ompl/geometric/planners/rrt/TRRT.h>
 #include <ompl/geometric/planners/rrt/pRRT.h>
 #include <ompl/geometric/planners/est/EST.h>
+
 #include <ompl/base/spaces/SE3StateSpace.h>
 #include <ompl/base/PlannerData.h>
 #include <cmath>
@@ -21,11 +23,11 @@
 #include <ostream>
 
 #include "TrajectoryPlannerSkel.h"
-#include "p4-arm-helper.h"
 #include "ManipulationPlanner_OMPL.h"
 #include <rtm/Manager.h>
 
 namespace og = ompl::geometric;
+namespace ob = ompl::base;
 
 struct JointLimit{
     double max;
@@ -42,18 +44,13 @@ class JointStateSampler{
 
     bool planWithSimpleSetup(const Manipulation::RobotJointInfo& startRobotJointInfo, const Manipulation::RobotJointInfo& goalRobotJointInfo, Manipulation::ManipulationPlan_out manipPlan);
 
-    void setComp(RTC::RtcBase* rtc){m_rtcomp = rtc;}
+    void setComp(ManipulationPlanner_OMPL* rtc){m_rtcomp = rtc;}
 
   protected:
 
     bool isStateValid(const ob::State *state);
-    void ForwardKinematics(const std::vector<TLink> &linkes,
-                           const std::vector<double> &angles, const TVector &base,
-                           std::vector<TVector> &result);
 
-    //ArmMeshCollisionChecker* m_armMeshCC;
-
-    RTC::RtcBase* m_rtcomp;
+    ManipulationPlanner_OMPL* m_rtcomp;
     int m_jointNum;
     int m_planningMethod = 1;
     std::vector<JointLimit> m_jointLimits;
